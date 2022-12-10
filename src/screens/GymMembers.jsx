341e4a89f3b70a128eaa521e98e6af61801b20b3
tanmaykapoor <tanmay.skater@gmail.com>
2022-12-09 19:44:44 -0500

    beautify dob members for admin view

--- src/screens/GymMembers.jsx
+++ src/screens/GymMembers.jsx
@@ -1,99 +1,106 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import Table from '../components/Table';
-import { MDBInput, MDBCol, MDBRow, MDBBtn } from 'mdb-react-ui-kit';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Table from "../components/Table";
+import { MDBInput, MDBCol, MDBRow, MDBBtn } from "mdb-react-ui-kit";
 
 function GymMembers() {
-  const user = JSON.parse(localStorage.getItem('user'))
+    const user = JSON.parse(localStorage.getItem("user"));
 
-  const [members, setMembers] = useState([]);
-  const [showEditForm, setShowEditForm] = useState(false);
+    const [members, setMembers] = useState([]);
+    const [showEditForm, setShowEditForm] = useState(false);
 
-  const displayEditForm = (e) => {
-    setShowEditForm(true);
-  }
+    const displayEditForm = (e) => {
+        setShowEditForm(true);
+    };
 
-  const getMembers = async() => {
-    try {
-      const res = await axios.get(`/gym/${user.gym_id}/members`);
-      res.data['dob'] = res.data['dob'].substring(0, 10)
-      setMembers(res.data);
-    }
-    catch(err) {
-      alert(err.response.data.msg || err);
-    }
-  }
+    const getMembers = async () => {
+        try {
+            const res = await axios.get(`/gym/${user.gym_id}/members`);
+            res.data.map((obj) => (obj.dob = obj.dob.substring(0, 10)));
+            setMembers(res.data);
+        } catch (err) {
+            alert(err.response.data.msg || err);
+        }
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    try {
-      // const res = axios.
-    } catch(err) {
-      alert(err.response.data.msg || err);
-    }
-    
-    setShowEditForm(false)
-  }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            // const res = axios.
+        } catch (err) {
+            alert(err.response.data.msg || err);
+        }
 
-  const deleteUser = async (e, email) => {
-    console.log(email);
-    try {
-      await axios.delete(`/user/${email}`)
-      getMembers();
-      alert("Member successfuly deleted !")
-    }
-    catch(err) {
-      alert(err.response.data.msg || err);
-    }
-  }
+        setShowEditForm(false);
+    };
 
+    const deleteUser = async (e, email) => {
+        console.log(email);
+        try {
+            await axios.delete(`/user/${email}`);
+            getMembers();
+            alert("Member successfuly deleted !");
+        } catch (err) {
+            alert(err.response.data.msg || err);
+        }
+    };
 
-  useEffect(() => {
-    getMembers();
-  }, [])
-  
-  console.log(members);
+    useEffect(() => {
+        getMembers();
+    }, []);
 
-  return (
-    
-    members.length > 0
-    ?
-    <div className='gym-members-div'>
-      <div className='edit-form-div' style={{display: showEditForm ? 'inline' : 'none'}}>
-        <form className='m-4 popup-form' onSubmit={handleSubmit}>
-          <MDBRow className='mb-4 w-3'>
-            <MDBCol>
-              <MDBInput label='First name' />
-            </MDBCol>
-            <MDBCol>
-              <MDBInput label='Last name' />
-            </MDBCol>
-          </MDBRow>
-          <MDBInput className='mb-4' type='email' label='Email address' />
-          <MDBInput className='mb-4' type='password' label='Password' />
+    console.log(members);
 
-          <MDBBtn type='submit' className='mb-0' block>
-            Update
-          </MDBBtn>
-          <MDBBtn color='danger' onClick={e => setShowEditForm(false)} className='mb-0' block>
-            Cancle
-          </MDBBtn>
-        </form>
-      </div>
+    return members.length > 0 ? (
+        <div className="gym-members-div">
+            <div
+                className="edit-form-div"
+                style={{ display: showEditForm ? "inline" : "none" }}
+            >
+                <form className="m-4 popup-form" onSubmit={handleSubmit}>
+                    <MDBRow className="mb-4 w-3">
+                        <MDBCol>
+                            <MDBInput label="First name" />
+                        </MDBCol>
+                        <MDBCol>
+                            <MDBInput label="Last name" />
+                        </MDBCol>
+                    </MDBRow>
+                    <MDBInput
+                        className="mb-4"
+                        type="email"
+                        label="Email address"
+                    />
+                    <MDBInput
+                        className="mb-4"
+                        type="password"
+                        label="Password"
+                    />
 
+                    <MDBBtn type="submit" className="mb-0" block>
+                        Update
+                    </MDBBtn>
+                    <MDBBtn
+                        color="danger"
+                        onClick={(e) => setShowEditForm(false)}
+                        className="mb-0"
+                        block
+                    >
+                        Cancle
+                    </MDBBtn>
+                </form>
+            </div>
 
-      <Table 
-        content='members' 
-        data={members} 
-        deleteItem={deleteUser} 
-        displayEditForm={displayEditForm}>  
-      </Table>
-
-    </div>
-    :
-    <div className='no-data'>No members in this gym yet</div>
-    
-  )
+            <Table
+                content="members"
+                data={members}
+                deleteItem={deleteUser}
+                displayEditForm={displayEditForm}
+            ></Table>
+        </div>
+    ) : (
+        <div className="no-data">No members in this gym yet</div>
+    );
 }
 
-export default GymMembers
+export default GymMembers;
