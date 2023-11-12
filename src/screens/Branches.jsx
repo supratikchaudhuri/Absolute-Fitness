9a2f913337ab8a6b6354abae287093a6f96181a3
supra <rikc.2000@gmail.com>
2023-11-12 14:53:26 -0500

    branches page

--- src/screens/Branches.jsx
+++ src/screens/Branches.jsx
@@ -1,113 +1,63 @@
-import React, { useEffect } from 'react';
-
-import {
-	MDBCard,
-	MDBCardImage,
-	MDBCardBody,
-	MDBCardTitle,
-	MDBCardText,
-	MDBRow,
-	MDBCol,
-	MDBBtn
-} from 'mdb-react-ui-kit';
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Branches() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const [branches, setBranches] = useState([])
+  const [branches, setBranches] = useState([]);
 
-	useEffect(() => {
-		const getBranches = async () => {
-			try {
-				const res = await axios.get("gym/")
-				await setBranches(res.data)
+  useEffect(() => {
+    const getBranches = async () => {
+      try {
+        const res = await axios.get("gym/");
+        setBranches(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-			} catch (err) {
-				console.log(err);
-			}
-		}
-
-		getBranches();
-		
-	}, [])
-	console.log(branches);
-
+    getBranches();
+  }, []);
 
   return (
-    <div className='branches-div'>
-      <h4>Absolute Fitness Branches</h4>
-      <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
-      {
-        branches.map(branch => (
-          <MDBCol>
-          <MDBCard className='h-100'	>
-            <MDBCardImage
-              src={branch.image_url}
-              alt='...'
-              position='top'
-            />
-            <MDBCardBody>
-              <MDBCardTitle>{branch.location}</MDBCardTitle>
-              <MDBCardText>
-                <p>Phone: {branch.phone}</p>
-								<p>{branch.state}</p>
-								<p>{branch.zip}</p>
-								<p>Monthly membership fee: ${branch.membership_fee}</p>
-              </MDBCardText>
+    <div id="branches-div" className="container">
+      <h4 className="center mt-3 mb-3">Absolute Fitness Branches</h4>
 
-							<div>
-								<MDBBtn onClick={e => navigate(`../gym/${branch.gym_id}/facilities`)}>View Facilities</MDBBtn>
-								<MDBBtn onClick={e => navigate(`../gym/${branch.gym_id}/trainers`)}>View Trainers</MDBBtn>
-							</div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        ))
-      }
-    </MDBRow>
+      <div className="row">
+        {branches.map((branch) => (
+          <div className="col-xs-12 col-md-4 mb-4">
+            <div className="card h-100">
+              <img src={branch.image_url} className="card-img-top" alt="..." />
+
+              <div className="card-body">
+                <h5 className="card-title">{branch.location}</h5>
+                <p>Phone: {branch.phone}</p>
+                <p>{branch.state}</p>
+                <p>{branch.zip}</p>
+                <p>Monthly membership fee: ${branch.membership_fee}</p>
+
+                <button
+                  className="btn btn-primary mt-2"
+                  onClick={(e) =>
+                    navigate(`../gym/${branch.gym_id}/facilities`)
+                  }
+                >
+                  View Facilities
+                </button>
+                <button
+                  className="btn btn-primary ms-2 mt-2"
+                  onClick={(e) => navigate(`../gym/${branch.gym_id}/trainers`)}
+                >
+                  View Trainers
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Branches
-
-
-
-
-
-// const branches = [
-	// 	{
-	// 		address: "123 ewqe eqweqwe, qweqwe",
-	// 		image: "https://www.tripsavvy.com/thmb/UfG0_2WB67pErEqfIQMvEjV4W20=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-947698310-1729da81e58f40058a9e45ba82532d57-2f992696318c42cbbd595ef3ec1043fd.jpg", 
-	// 		city: "LA", 
-	// 		state: "CA", 
-	// 		zip: 1111
-	// 	}, {
-	// 		address: "123 ewqe eqweqwe, qweqwe",
-	// 		image: "https://www.travelandleisure.com/thmb/ATskoUEBblzpBKlym1lPV4J1m-s=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/san-diego-california-SDTG0221-7d1cfd65a826426d8cc7f6e41345ac19.jpg", 
-	// 		city: "San Diego", 
-	// 		state: "CA", 
-	// 		zip: 1111
-	// 	}, {
-	// 		address: "123 ewqe eqweqwe, qweqwe",
-	// 		image: "https://cdn.getyourguide.com/img/location/533597d7653a9.jpeg/99.jpg", 
-	// 		city: "Boston", 
-	// 		state: "MA", 
-	// 		zip: 1111
-	// 	}, {
-	// 		address: "123 ewqe eqweqwe, qweqwe",
-	// 		image: "https://media.nomadicmatt.com/2022/newnycguidemain.jpeg", 
-	// 		city: "New York City", 
-	// 		state: "NY", 
-	// 		zip: 1111
-	// 	}, {
-	// 		address: "123 ewqe eqweqwe, qweqwe",
-	// 		image: "https://www.gannett-cdn.com/presto/2022/06/01/NAAS/f757462d-1524-4040-8512-7ecf9ee3b391-austin_skyline.JPG", 
-	// 		city: "Austin", 
-	// 		state: "TX", 
-	// 		zip: 1111
-	// 	}
-	// ]
+export default Branches;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,20 +1,12 @@
-import React from 'react'
-import Table from '../components/Table'
-import Branches from './Branches';
+import React from "react";
+import Branches from "./Branches";
 
 function Home() {
-	var cols = ["col1", "col2", "col3"];
-	const rows = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["10", "12", "13"], ["21", "22", "23"], ["31", "32", "33"]];
-
   return (
     <>
-
-    {/* <Table cols={cols} rows = {rows}></Table> */}
-
-    <Branches/>
+      <Branches />
     </>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
