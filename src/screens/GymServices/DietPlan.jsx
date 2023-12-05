2c169af785db8405345aaff107c3000d4807d13d
supra <rikc.2000@gmail.com>
2023-12-05 13:13:44 -0500

    some error handeling

--- src/screens/GymServices/DietPlan.jsx
+++ src/screens/GymServices/DietPlan.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { getDietPlanForUser } from "../../api/dietPlan";
+import AlertBox from "../../components/AlertBox";
 
 function DietPlan() {
   const [mealPlan, setMealPlan] = useState(null);
@@ -15,44 +16,47 @@ function DietPlan() {
     getMeals();
   }, []);
 
-  return (
-    mealPlan && (
-      <div className="container d-flex flex-column align-items-center">
-        <h4 className="page-title">{mealPlan.description}</h4>
-        <div className="workout-plan mt-3">
-          <h5 className="center">Today's Meals</h5>
+  return mealPlan ? (
+    <div className="container d-flex flex-column align-items-center">
+      <h4 className="page-title">{mealPlan.description}</h4>
+      <div className="workout-plan mt-3">
+        <h5 className="center">Today's Meals</h5>
 
-          <div className="meals-div row">
-            {mealPlan.meals.map((meal, index) => (
-              <div
-                className="col-xs-12 col-md-4 mt-3 ms-3 p-0 fit-content"
-                key={index}
-              >
-                <div className="curved-border">
-                  <img
-                    src={meal.imageUrl}
-                    alt="meal"
-                    style={{ "max-width": "400px" }}
-                    className="w-100"
-                  />
-                  <div className="p-3">
-                    <h5>
-                      {index === 0
-                        ? "Breakfast"
-                        : index === 1
-                        ? "Lunch"
-                        : "Dinner"}
-                    </h5>
-                    <p>{meal.meal}</p>
-                    <p className="text-muted">Calories: {meal.calories}</p>
-                  </div>
+        <div className="meals-div row">
+          {mealPlan.meals.map((meal, index) => (
+            <div
+              className="col-xs-12 col-md-4 mt-3 ms-3 p-0 fit-content"
+              key={index}
+            >
+              <div className="curved-border">
+                <img
+                  src={meal.imageUrl}
+                  alt="meal"
+                  style={{ "max-width": "400px" }}
+                  className="w-100"
+                />
+                <div className="p-3">
+                  <h5>
+                    {index === 0
+                      ? "Breakfast"
+                      : index === 1
+                      ? "Lunch"
+                      : "Dinner"}
+                  </h5>
+                  <p>{meal.meal}</p>
+                  <p className="text-muted">Calories: {meal.calories}</p>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
-    )
+    </div>
+  ) : (
+    <AlertBox
+      type="warning"
+      message="You don't have a meal plan yet. Please contact your trainer."
+    />
   );
 }
 
--- src/screens/GymServices/WorkoutPlan.jsx
+++ src/screens/GymServices/WorkoutPlan.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getWorkoutPlanForUser } from "../../api/workoutPlan";
+import AlertBox from "../../components/AlertBox";
 
 function WorkoutPlan() {
   const [workoutPlan, setWorkoutPlan] = useState(null);
@@ -16,38 +17,41 @@ function WorkoutPlan() {
 
   console.log(workoutPlan);
 
-  return (
-    workoutPlan && (
-      <div className="container d-flex flex-column align-items-center">
-        <h4 className="page-title mt-2">Today's exceerecises</h4>
-        <h5 className=" center mt-2">{workoutPlan.description}</h5>
+  return workoutPlan ? (
+    <div className="container d-flex flex-column align-items-center">
+      <h4 className="page-title mt-2">Today's exceerecises</h4>
+      <h5 className=" center mt-2">{workoutPlan.description}</h5>
 
-        <div className="workouts-div row">
-          {workoutPlan.excercises.map((exc, index) => (
-            <div
-              className="col-xs-12 col-md-4 mt-3 ms-3 p-0 fit-content"
-              key={index}
-            >
-              <div className="curved-border">
-                <img
-                  src={exc.imageUrl}
-                  alt="excercise"
-                  className="w-100"
-                  style={{ "max-width": "400px" }}
-                />
-                <div className="p-3">
-                  <h5 className="">{exc.excercise}</h5>
-                  <p>
-                    {exc.reps} Sets {exc.sets} Reps ea.
-                  </p>
-                  <p className="">You'll burn approximately "xx" calories.</p>
-                </div>
+      <div className="workouts-div row">
+        {workoutPlan.excercises.map((exc, index) => (
+          <div
+            className="col-xs-12 col-md-4 mt-3 ms-3 p-0 fit-content"
+            key={index}
+          >
+            <div className="curved-border">
+              <img
+                src={exc.imageUrl}
+                alt="excercise"
+                className="w-100"
+                style={{ "max-width": "400px" }}
+              />
+              <div className="p-3">
+                <h5 className="">{exc.excercise}</h5>
+                <p>
+                  {exc.reps} Sets {exc.sets} Reps ea.
+                </p>
+                <p className="">You'll burn approximately "xx" calories.</p>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    )
+    </div>
+  ) : (
+    <AlertBox
+      type="warning"
+      message="You don't have a workout plan yet. Please contact your trainer."
+    />
   );
 }
 
