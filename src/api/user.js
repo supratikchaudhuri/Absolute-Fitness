6d3cedfd7102703e48fca9955abb03d41762e381
tanmaykapoor <tanmay.skater@gmail.com>
2023-12-06 18:57:38 -0500

    stupid supra error

--- src/api/user.js
+++ src/api/user.js
@@ -3,87 +3,91 @@ import api from "./axiosConfig";
 const user = JSON.parse(localStorage.getItem("user"));
 
 export const signup = async (userDetails) => {
-  try {
-    const res = await api.post("user/signup", userDetails);
-    if (res.status === 200) {
-      localStorage.setItem("user", JSON.stringify(res.data));
+    try {
+        const res = await api.post("user/signup", userDetails);
+        if (res.status === 200) {
+            localStorage.setItem("user", JSON.stringify(res.data));
+        }
+        return res.status;
+    } catch (err) {
+        console.log(err);
     }
-    return res.status;
-  } catch (err) {
-    console.log(err);
-  }
 };
 
 export const getAllUsers = async () => {
-  try {
-    const res = await api.get("user/", {
-      headers: {
-        Authorization: `Bearer ${user.accessToken}`,
-      },
-    });
-    return res.data;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const res = await api.get("user/", {
+            headers: {
+                Authorization: `Bearer ${user.accessToken}`,
+            },
+        });
+        return res.data;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 export const getUser = async (username) => {
-  try {
-    const res = await api.get(`user/${username}`);
-    return res.data;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const res = await api.get(`user/${username}`);
+        return res.data;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 export const updateUser = async (userDetails) => {
-  try {
-    console.log(userDetails);
-    const res = await api.put(`user/${userDetails.username}`, userDetails, {
-      headers: {
-        Authorization: `Bearer ${user.accessToken}`,
-      },
-    });
-    if (res.status === 200) {
-      localStorage.setItem("user", JSON.stringify(res.data));
+    try {
+        console.log(userDetails);
+        const res = await api.put(`user/${userDetails.username}`, userDetails, {
+            headers: {
+                Authorization: `Bearer ${user.accessToken}`,
+            },
+        });
+        if (res.status === 200) {
+            localStorage.setItem("user", JSON.stringify(res.data));
+        }
+        return res.status;
+    } catch (err) {
+        console.log(err);
+        return err.response.status;
     }
-    return res.status;
-  } catch (err) {
-    console.log(err);
-    return err.response.status;
-  }
 };
 
 export const deleteGymUser = async (userId) => {
-  try {
-    const res = await api.delete(`user/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${user.accessToken}`,
-      },
-    });
-    return res.status;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const res = await api.delete(`user/${userId}`, {
+            headers: {
+                Authorization: `Bearer ${user.accessToken}`,
+            },
+        });
+        return res.status;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 export const paymentSuccessful = async (paymentToken) => {
-  try {
-    const res = await api.post("user/payment", paymentToken, {
-      headers: {
-        Authorization: `Bearer ${user.accessToken}`,
-      },
-    });
+    try {
+        const res = await api.post(
+            "user/payment",
+            { paymentToken },
+            {
+                headers: {
+                    Authorization: `Bearer ${user.accessToken}`,
+                },
+            }
+        );
 
-    const accessToken = res.data.accessToken;
-    console.log(user.accessToken === accessToken);
-    user = { ...user, accessToken };
-    if (res.status === 200) {
-      localStorage.setItem("user", JSON.stringify(user));
+        const accessToken = res.data.accessToken;
+        console.log(user.accessToken === accessToken);
+        user = { ...user, accessToken };
+        if (res.status === 200) {
+            localStorage.setItem("user", JSON.stringify(user));
+        }
+        user = localStorage.getItem("user");
+        console.log(user.accessToken === accessToken);
+    } catch (err) {
+        console.log(err);
     }
-    user = localStorage.getItem("user");
-    console.log(user.accessToken === accessToken);
-  } catch (err) {
-    console.log(err);
-  }
 };
--- src/screens/Pricing/PaymentSuccessFul.jsx
+++ src/screens/Pricing/PaymentSuccessFul.jsx
@@ -1,32 +1,34 @@
 import React, { useEffect } from "react";
 import gif from "../../Images/doggoGif.gif";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { paymentSuccessful } from "../../api/user";
 
 const PaymentSuccessFul = () => {
-  const { token: paymentToken } = useParams();
+    // const { token: paymentToken } = useParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const paymentToken = searchParams.get("token");
 
-  const paymentSuccessfulByUser = async (paymentToken) => {
-    await paymentSuccessful(paymentToken);
-  };
+    const paymentSuccessfulByUser = async (paymentToken) => {
+        await paymentSuccessful(paymentToken);
+    };
 
-  useEffect(() => {
-    paymentSuccessfulByUser();
-  }, []);
-  return (
-    <div className="container ">
-      <div className="card text-center shadow p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <img src={gif} alt="gif" />
-          <h1 className="card-title mt-1">Payment Successful!</h1>
-          <p className="card-text">Thank you for your purchase.</p>
-          <a href="/" className="btn btn-primary">
-            Back to Home
-          </a>
+    useEffect(() => {
+        paymentSuccessfulByUser(paymentToken);
+    }, []);
+    return (
+        <div className="container ">
+            <div className="card text-center shadow p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+                    <img src={gif} alt="gif" />
+                    <h1 className="card-title mt-1">Payment Successful!</h1>
+                    <p className="card-text">Thank you for your purchase.</p>
+                    <a href="/" className="btn btn-primary">
+                        Back to Home
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PaymentSuccessFul;
