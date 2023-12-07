e6e4ed4b010b1061320f0c9fae2613fae6a07a9f
tanmaykapoor <tanmay.skater@gmail.com>
2023-12-06 21:31:48 -0500

    fix reset password route

--- src/App.js
+++ src/App.js
@@ -47,136 +47,202 @@ import EditGym from "./screens/Gym/EditGym";
 /************************ Imports complete ************************/
 
 const GOOGLE_CLIENT_ID =
-  "771432891086-pnekaemhkjo6f60ndfu1l8heh7u64c01.apps.googleusercontent.com";
+    "771432891086-pnekaemhkjo6f60ndfu1l8heh7u64c01.apps.googleusercontent.com";
 
 const STRIPE_PUBLISHABLE_KEY =
-  "pk_test_51N4IuESIsW8FsEuEN6OPvDtj95w1XrlO17O9gLaFcrDTRDQWc5CdCCHeqgKznFdcQAGhuHLbzDsFD1NJIlMpFgdW00iQ2mlzNY";
+    "pk_test_51N4IuESIsW8FsEuEN6OPvDtj95w1XrlO17O9gLaFcrDTRDQWc5CdCCHeqgKznFdcQAGhuHLbzDsFD1NJIlMpFgdW00iQ2mlzNY";
 const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
 function App() {
-  return (
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        {![
-          "/login",
-          "/signup",
-          "/staff-login",
-          "/forgot-password",
-          "/resetPassword",
-        ].some((path) => window.location.pathname.includes(path)) && <Navbar />}
+    return (
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            <BrowserRouter>
+                {![
+                    "/login",
+                    "/signup",
+                    "/staff-login",
+                    "/forgot-password",
+                    "/resetPassword",
+                ].some((path) => window.location.pathname.includes(path)) && (
+                    <Navbar />
+                )}
 
-        <div className="App">
-          <Routes>
-            <Route exact path="/login" element={<LoginScreen />} />
-            <Route exact path="/forgot-password" element=<ForgotPassword /> />
-            <Route
-              exact
-              path="/user/:username/resetPassword/:token"
-              element=<SetupNewPassword />
-            />
-            <Route exact path="/signup" element={<Signup />} />
+                <div className="App">
+                    <Routes>
+                        <Route exact path="/login" element={<LoginScreen />} />
+                        <Route
+                            exact
+                            path="/forgot-password"
+                            element=<ForgotPassword />
+                        />
+                        <Route
+                            exact
+                            path="/resetPassword/:token"
+                            element=<SetupNewPassword />
+                        />
+                        <Route exact path="/signup" element={<Signup />} />
 
-            <Route
-              exact
-              path="/user/profile/:username"
-              element={<PublicProfile />}
-            />
+                        <Route
+                            exact
+                            path="/user/profile/:username"
+                            element={<PublicProfile />}
+                        />
 
-            <Route exact path="/home" element={<Home />} />
-            <Route exact path="/branches" element={<Branches />} />
-            <Route exact path="/gym/:gym_id" element={<Franchise />} />
-            <Route exact path="/gym/edit-gym/:gym_id" element={<EditGym />} />
+                        <Route exact path="/home" element={<Home />} />
+                        <Route exact path="/branches" element={<Branches />} />
+                        <Route
+                            exact
+                            path="/gym/:gym_id"
+                            element={<Franchise />}
+                        />
+                        <Route
+                            exact
+                            path="/gym/edit-gym/:gym_id"
+                            element={<EditGym />}
+                        />
 
-            <Route exact path="/dashboard" element={<Dashboard />} />
+                        <Route
+                            exact
+                            path="/dashboard"
+                            element={<Dashboard />}
+                        />
 
-            <Route exact path="/gym/:gym_id/trainers" element={<Trainers />} />
-            <Route
-              exact
-              path="/gym/:gym_id/facilities"
-              element={<Facilities />}
-            />
-            <Route
-              exact
-              path="/gym/:gym_id/equipments"
-              element={<GymEquipments />}
-            />
+                        <Route
+                            exact
+                            path="/gym/:gym_id/trainers"
+                            element={<Trainers />}
+                        />
+                        <Route
+                            exact
+                            path="/gym/:gym_id/facilities"
+                            element={<Facilities />}
+                        />
+                        <Route
+                            exact
+                            path="/gym/:gym_id/equipments"
+                            element={<GymEquipments />}
+                        />
 
-            <Route
-              exact
-              path="/health-record"
-              element={<MemberHealthRecord />}
-            />
+                        <Route
+                            exact
+                            path="/health-record"
+                            element={<MemberHealthRecord />}
+                        />
 
-            <Route exact path="/gym/:gymId/members" element={<GymMembers />} />
-            <Route exact path="/gym/:gymId/staff" element={<GymStaff />} />
+                        <Route
+                            exact
+                            path="/gym/:gymId/members"
+                            element={<GymMembers />}
+                        />
+                        <Route
+                            exact
+                            path="/gym/:gymId/staff"
+                            element={<GymStaff />}
+                        />
 
-            <Route
-              exact
-              path="/my-subscriptions"
-              element={<MySubscriptionsPage />}
-            />
+                        <Route
+                            exact
+                            path="/my-subscriptions"
+                            element={<MySubscriptionsPage />}
+                        />
 
-            <Route element={<PrivateRoute />}>
-              <Route
-                exact
-                path="/pricing-plans"
-                element={
-                  <Elements stripe={stripePromise}>
-                    <MembershipPricingPlans />
-                  </Elements>
-                }
-              />
+                        <Route element={<PrivateRoute />}>
+                            <Route
+                                exact
+                                path="/pricing-plans"
+                                element={
+                                    <Elements stripe={stripePromise}>
+                                        <MembershipPricingPlans />
+                                    </Elements>
+                                }
+                            />
 
-              <Route exact path="/user/profile" element={<PrivateProfile />} />
-              <Route
-                exact
-                path="/user/edit-profile"
-                element={<EditProfile />}
-              />
+                            <Route
+                                exact
+                                path="/user/profile"
+                                element={<PrivateProfile />}
+                            />
+                            <Route
+                                exact
+                                path="/user/edit-profile"
+                                element={<EditProfile />}
+                            />
 
-              <Route
-                exact
-                path="/health-record"
-                element={<MemberHealthRecord />}
-              />
+                            <Route
+                                exact
+                                path="/health-record"
+                                element={<MemberHealthRecord />}
+                            />
 
-              {/* <Route index path="/nutrition" element={<NutrionIX />} /> */}
+                            {/* <Route index path="/nutrition" element={<NutrionIX />} /> */}
 
-              <Route
-                exact
-                path="/trainer/:staffId/memberRecords"
-                element={<TrainerPerformance />}
-              />
+                            <Route
+                                exact
+                                path="/trainer/:staffId/memberRecords"
+                                element={<TrainerPerformance />}
+                            />
 
-              {/* Route for Subscribers */}
-              <Route element={<SubscriberRoute />}>
-                <Route exact path="/health-plan" element={<HealthPlan />} />
-                <Route exact path="/workout-plan" element={<WorkoutPlan />} />
-                <Route exact path="/diet-plan" element={<DietPlan />} />
-                <Route exact path="/nutrition" element={<NutrionIX />} />
-              </Route>
-              <Route
-                exact
-                path="/payment-success"
-                element={<PaymentSuccessFul />}
-              />
+                            {/* Route for Subscribers */}
+                            <Route element={<SubscriberRoute />}>
+                                <Route
+                                    exact
+                                    path="/health-plan"
+                                    element={<HealthPlan />}
+                                />
+                                <Route
+                                    exact
+                                    path="/workout-plan"
+                                    element={<WorkoutPlan />}
+                                />
+                                <Route
+                                    exact
+                                    path="/diet-plan"
+                                    element={<DietPlan />}
+                                />
+                                <Route
+                                    exact
+                                    path="/nutrition"
+                                    element={<NutrionIX />}
+                                />
+                            </Route>
+                            <Route
+                                exact
+                                path="/payment-success"
+                                element={<PaymentSuccessFul />}
+                            />
 
-              {/* Route for Root */}
-              <Route element={<OwnerRoute />}>
-                <Route exact path="/all-members" element={<AllMembers />} />
-                <Route exact path="/all-staff" element={<AllStaff />} />
-              </Route>
-              <Route exact path="/gym/addGym" element={<AddGymBranch />} />
-            </Route>
+                            {/* Route for Root */}
+                            <Route element={<OwnerRoute />}>
+                                <Route
+                                    exact
+                                    path="/all-members"
+                                    element={<AllMembers />}
+                                />
+                                <Route
+                                    exact
+                                    path="/all-staff"
+                                    element={<AllStaff />}
+                                />
+                            </Route>
+                            <Route
+                                exact
+                                path="/gym/addGym"
+                                element={<AddGymBranch />}
+                            />
+                        </Route>
 
-            {/* tema members in root */}
-            <Route exact path="/" element={<Navigate to="/home" />} />
-            {/* <Route path="*" element={<PageNotFound />} /> */}
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </GoogleOAuthProvider>
-  );
+                        {/* tema members in root */}
+                        <Route
+                            exact
+                            path="/"
+                            element={<Navigate to="/home" />}
+                        />
+                        {/* <Route path="*" element={<PageNotFound />} /> */}
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </GoogleOAuthProvider>
+    );
 }
 
 export default App;
