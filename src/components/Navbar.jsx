1383bd3410a4c119cee681f3c965333266314d43
tanmaykapoor <tanmay.skater@gmail.com>
2023-12-06 22:15:26 -0500

    navbar redirect fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,242 +2,261 @@ import React from "react";
 import { useJwt } from "react-jwt";
 
 function Navbar() {
-  const logout = () => {
-    localStorage.clear();
-    window.location.href = "/login";
-  };
-
-  let user = JSON.parse(localStorage.getItem("user")) || {};
-
-  const { decodedToken } = useJwt(user.accessToken);
-
-  if (decodedToken) {
-    user = {
-      ...user,
-      type: decodedToken.type,
-      subscribed: decodedToken.subscribed || false,
+    const logout = () => {
+        localStorage.clear();
+        window.location.href = "/login";
     };
-    localStorage.setItem("user", JSON.stringify(user));
-  }
 
-  const path = window.location.pathname;
+    let user = JSON.parse(localStorage.getItem("user")) || {};
 
-  const navBarLink = [
-    {
-      name: "Home",
-      link: "/home",
-    },
-    {
-      name: "Pricing",
-      link: "/pricing-plans",
-    },
-    {
-      name: "Check Nutrition",
-      link: "/nutrition",
-    },
-  ];
+    const { decodedToken } = useJwt(user.accessToken);
 
-  const adminLinks = [
-    {
-      name: "My Gym",
-      link: `/gym/${user.gym_id}`,
-    },
-  ];
+    if (decodedToken) {
+        user = {
+            ...user,
+            type: decodedToken.type,
+            subscribed: decodedToken.subscribed || false,
+        };
+        localStorage.setItem("user", JSON.stringify(user));
+    }
 
-  const trainerLinks = [
-    {
-      name: "Client",
-      link: "/client",
-    },
-  ];
+    const path = window.location.pathname;
 
-  const memberLinks = [
-    {
-      name: "Health Record",
-      link: "/health-record",
-    },
-    {
-      name: "Health Plan",
-      link: "/health-plan",
-    },
-    { name: "Workout Plan", link: "/workout-plan" },
-    { name: "Diet Plan", link: "/diet-plan" },
-  ];
+    const navBarLink = [
+        {
+            name: "Home",
+            link: "/home",
+        },
+        {
+            name: "Pricing",
+            link: "/pricing-plans",
+        },
+        {
+            name: "Check Nutrition",
+            link: "/nutrition",
+        },
+    ];
 
-  const rootLinks = [
-    {
-      name: "Members",
-      link: "/all-members",
-    },
-    {
-      name: "Staff",
-      link: "/all-staff",
-    },
-  ];
+    const adminLinks = [
+        {
+            name: "My Gym",
+            link: `/gym/${user.gym_id}`,
+        },
+    ];
 
-  return (
-    <>
-      <nav className="custom-navbar fixed-top navbar navbar-dark bg-primary navbar-expand-lg">
-        <a className="navbar-brand" href=".">
-          <span>Absolute Fitness</span>{" "}
-          <i className="ms-2 fa-solid fa-dumbbell"></i>
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#af-navbar"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          style={{ border: "1px solid white" }}
-        >
-          <i className="fa-solid fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="af-navbar">
-          <ul className="navbar-nav">
-            {/* common links */}
-            {navBarLink.map((item) => {
-              return (
-                <li className="nav-item">
-                  <a
-                    className={`nav-link ${
-                      path.includes(item.link) ? "active" : ""
-                    }`}
-                    href={item.link}
-                  >
-                    {item.name}
-                  </a>
-                </li>
-              );
-            })}
+    const trainerLinks = [
+        {
+            name: "Client",
+            link: "/client",
+        },
+    ];
 
-            {user.accessToken && (
-              <>
-                {/* admin links */}
-                {user.type === "admin" &&
-                  adminLinks.map((item) => {
-                    return (
-                      <li className="nav-item">
-                        <a
-                          className={`nav-link ${
-                            path.includes(item.link) ? "active" : ""
-                          }`}
-                          href={item.link}
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    );
-                  })}
+    const memberLinks = [
+        {
+            name: "Health Record",
+            link: "/health-record",
+        },
+        {
+            name: "Health Plan",
+            link: "/health-plan",
+        },
+        { name: "Workout Plan", link: "/workout-plan" },
+        { name: "Diet Plan", link: "/diet-plan" },
+    ];
 
-                {/* trainer links */}
-                {user.type === "trainer" &&
-                  trainerLinks.map((item) => {
-                    return (
-                      <li className="nav-item">
-                        <a
-                          className={`nav-link ${
-                            path.includes(item.link) ? "active" : ""
-                          }`}
-                          href={item.link}
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    );
-                  })}
+    const rootLinks = [
+        {
+            name: "Members",
+            link: "/all-members",
+        },
+        {
+            name: "Staff",
+            link: "/all-staff",
+        },
+    ];
 
-                {/* customer links */}
-                {user.type === "member" &&
-                  memberLinks.map((item) => {
-                    return (
-                      <li className="nav-item">
-                        <a
-                          className={`nav-link ${
-                            path.includes(item.link) ? "active" : ""
-                          }`}
-                          href={item.link}
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    );
-                  })}
-
-                {/* root links */}
-                {user.type === "root" &&
-                  rootLinks.map((item) => {
-                    return (
-                      <li className="nav-item">
-                        <a
-                          className={`nav-link ${
-                            path.includes(item.link) ? "active" : ""
-                          }`}
-                          href={item.link}
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    );
-                  })}
-              </>
-            )}
-          </ul>
-
-          {user.accessToken ? (
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="."
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
+    return (
+        <>
+            <nav className="custom-navbar fixed-top navbar navbar-dark bg-primary navbar-expand-lg">
+                <a className="navbar-brand" href="/">
+                    <span>Absolute Fitness</span>{" "}
+                    <i className="ms-2 fa-solid fa-dumbbell"></i>
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#af-navbar"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    style={{ border: "1px solid white" }}
                 >
-                  {user.name}
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="/user/profile">
-                    Profile
-                  </a>
-                  {user.type === "customer" && (
-                    <a className="dropdown-item" href="/payment">
-                      My Subscription
-                    </a>
-                  )}
-                  <div className="dropdown-divider"></div>
-                  <p
-                    className="dropdown-item mb-0 pointer"
-                    onClick={(e) => {
-                      var ans = window.confirm(
-                        "Are you sure you want to log out?"
-                      );
-                      if (ans) {
-                        logout();
-                      }
-                    }}
-                  >
-                    Log Out
-                  </p>
+                    <i className="fa-solid fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="af-navbar">
+                    <ul className="navbar-nav">
+                        {/* common links */}
+                        {navBarLink.map((item) => {
+                            return (
+                                <li className="nav-item">
+                                    <a
+                                        className={`nav-link ${
+                                            path.includes(item.link)
+                                                ? "active"
+                                                : ""
+                                        }`}
+                                        href={item.link}
+                                    >
+                                        {item.name}
+                                    </a>
+                                </li>
+                            );
+                        })}
+
+                        {user.accessToken && (
+                            <>
+                                {/* admin links */}
+                                {user.type === "admin" &&
+                                    adminLinks.map((item) => {
+                                        return (
+                                            <li className="nav-item">
+                                                <a
+                                                    className={`nav-link ${
+                                                        path.includes(item.link)
+                                                            ? "active"
+                                                            : ""
+                                                    }`}
+                                                    href={item.link}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            </li>
+                                        );
+                                    })}
+
+                                {/* trainer links */}
+                                {user.type === "trainer" &&
+                                    trainerLinks.map((item) => {
+                                        return (
+                                            <li className="nav-item">
+                                                <a
+                                                    className={`nav-link ${
+                                                        path.includes(item.link)
+                                                            ? "active"
+                                                            : ""
+                                                    }`}
+                                                    href={item.link}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            </li>
+                                        );
+                                    })}
+
+                                {/* customer links */}
+                                {user.type === "member" &&
+                                    memberLinks.map((item) => {
+                                        return (
+                                            <li className="nav-item">
+                                                <a
+                                                    className={`nav-link ${
+                                                        path.includes(item.link)
+                                                            ? "active"
+                                                            : ""
+                                                    }`}
+                                                    href={item.link}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            </li>
+                                        );
+                                    })}
+
+                                {/* root links */}
+                                {user.type === "root" &&
+                                    rootLinks.map((item) => {
+                                        return (
+                                            <li className="nav-item">
+                                                <a
+                                                    className={`nav-link ${
+                                                        path.includes(item.link)
+                                                            ? "active"
+                                                            : ""
+                                                    }`}
+                                                    href={item.link}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            </li>
+                                        );
+                                    })}
+                            </>
+                        )}
+                    </ul>
+
+                    {user.accessToken ? (
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item dropdown">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    href="."
+                                    id="navbarDropdown"
+                                    role="button"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false"
+                                >
+                                    {user.name}
+                                </a>
+                                <div
+                                    className="dropdown-menu"
+                                    aria-labelledby="navbarDropdown"
+                                >
+                                    <a
+                                        className="dropdown-item"
+                                        href="/user/profile"
+                                    >
+                                        Profile
+                                    </a>
+                                    {user.type === "customer" && (
+                                        <a
+                                            className="dropdown-item"
+                                            href="/payment"
+                                        >
+                                            My Subscription
+                                        </a>
+                                    )}
+                                    <div className="dropdown-divider"></div>
+                                    <p
+                                        className="dropdown-item mb-0 pointer"
+                                        onClick={(e) => {
+                                            var ans = window.confirm(
+                                                "Are you sure you want to log out?"
+                                            );
+                                            if (ans) {
+                                                logout();
+                                            }
+                                        }}
+                                    >
+                                        Log Out
+                                    </p>
+                                </div>
+                            </li>
+                        </ul>
+                    ) : (
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">
+                                    Login
+                                </a>
+                            </li>
+                        </ul>
+                    )}
                 </div>
-              </li>
-            </ul>
-          ) : (
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="/login">
-                  Login
-                </a>
-              </li>
-            </ul>
-          )}
-        </div>
-      </nav>
-    </>
-  );
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
